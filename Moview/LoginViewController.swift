04da3dd403db1cf72a6231b608783544b01e67c3
Sarah-Lee <sarahlee.elhadad@gmail.com>
2021-07-03 22:37:09 +0300

    Create and design login and register screens

---
+++ Moview/LoginViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LoginViewController.swift
+//  Moview
+//
+//  Created by admin on 03/07/2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func backToLogin(segue: UIStoryboardSegue) {
+        
+    }
+
+}
---
+++ Moview/RegisterViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RegisterViewController.swift
+//  Moview
+//
+//  Created by admin on 03/07/2021.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var fullNameText: UITextField!
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
+    @IBAction func registerButton(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
