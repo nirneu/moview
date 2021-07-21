4f4763dab847a3825930d9277583e99109c5f2cb
Nir Neuman <nir.neumann@gmail.com>
2021-07-21 22:48:49 +0300

    Review details is working perfectly

--- Moview/ReviewDetailsViewController.swift
+++ Moview/ReviewDetailsViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Kingfisher
 
 class ReviewDetailsViewController: UIViewController {
 
@@ -14,23 +15,22 @@ class ReviewDetailsViewController: UIViewController {
     @IBOutlet weak var movieTitle: UILabel!
     @IBOutlet weak var year: UILabel!
     @IBOutlet weak var genre: UILabel!
-    @IBOutlet weak var summary: UITextView!
+    @IBOutlet weak var review: UITextView!
+    
+    var reviewObejct: Review?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let user = Model.instance.getUser(byId: reviewObejct!.userId!) {
+            userName.text = user.fullName
+        }
+        movieTitle.text = reviewObejct!.movieName
+        year.text = String(reviewObejct!.releaseYear)
+        genre.text = reviewObejct!.genre
+        posterImage.kf.setImage(with: URL(string: reviewObejct!.imageUrl!),placeholder: UIImage(named: "Default Avatar"))
+        review.text = reviewObejct!.review
 
-        // Do any additional setup after loading the view.
+       
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
